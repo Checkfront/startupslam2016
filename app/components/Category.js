814541ff77fb8448c868d3e187f2260c676863b5
Marc Alonzo <marc@checkfront.com>
2016-09-30 16:28:17 -0700

    Added slides

--- app/components/Category.js
+++ app/components/Category.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
+const imageStyles = {
+	borderRadius: '6px'
+}
+
 function Category({name, description, image}) {
 	return (
 		<div className='col-sm-6'>
 			<div className='thumbnail'>
-				<img className='img-responsive img-rounded' src={image} />
+				<img style={imageStyles} src={image} />
 				<div className='caption'>
 					<h3>{name}</h3>
 					<p>{description}</p>
